fed6cf1142628ee9e689372bb02f80b4d45e1146
Vlad Shynder <shynderv@gmail.com>
2020-11-14 18:48:56 +0200

    loader reducer added

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from './App';
 
 import { configureStore } from '@reduxjs/toolkit';
 import { Provider } from 'react-redux';
-import rootReducer from './redux/reduxers';
+import { rootReducer } from './redux';
 
 const store = configureStore({
   reducer: rootReducer,
