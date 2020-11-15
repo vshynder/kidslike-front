f2dcc68a06766dfe09d073737f4b95de693fd73e
velmor99 <volgagaz24100@gmail.com>
2020-11-15 19:02:44 +0200

    html/css done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React from 'react';
+/* AddChildForm & Sidebar */
+import AddChildForm from './components/AddChildForm/Form'
+import Sidebar from './components/IformationOnChild_(sidebar)/InformationList'
 
 function App() {
   return <div className="App">Hello</div>;
