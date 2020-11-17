3edbc61d26e36b3b94fb26d6a6872737cb781559
Andrii Makohin <andreas2010@ukr.net>
2020-11-17 18:05:42 +0200

    add_prettierr

--- src/components/AuthPageNavigation/AuthPageNavigation.js
+++ src/components/AuthPageNavigation/AuthPageNavigation.js
@@ -2,33 +2,34 @@ import React from 'react';
 import SiginUpForm from '../SiginUpForm/SiginUpForm';
 
 // Styles
-import styles from './AuthPageNavigation.module.css'
+import styles from './AuthPageNavigation.module.css';
 
-const {container,
-    logo,
-    siginUpForm,
-    siginUpForm__wraper,
-    siginUpForm__titel,
-    authDesktop,
-    authDesktop__wraper,
-    authDesktop__titel,
-    authMain,
-    authPresents,
-    authSasha
-} = styles
+const {
+  container,
+  logo,
+  siginUpForm,
+  siginUpForm__wraper,
+  siginUpForm__titel,
+  authDesktop,
+  authDesktop__wraper,
+  authDesktop__titel,
+  authMain,
+  authPresents,
+  authSasha,
+} = styles;
 
 function AuthPageNavigation() {
   return (
     <div className={container}>
       <section className={siginUpForm}>
-      <div className={logo}>
-        <h1>Logo</h1>
-      </div>
+        <div className={logo}>
+          <h1>Logo</h1>
+        </div>
         <div className={siginUpForm__wraper}>
           <h2 className={siginUpForm__titel}>
             Мотивуйте дитину вдосконалювати себе!
           </h2>
-        <SiginUpForm />
+          <SiginUpForm />
         </div>
       </section>
 
