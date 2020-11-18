dcbee36f6aaef34fee521f2a33ebfb370dba6af5
Vlad Shynder <shynderv@gmail.com>
2020-11-18 20:25:06 +0200

    browser router added index

--- src/index.js
+++ src/index.js
@@ -1,27 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+
 import { configureStore } from '@reduxjs/toolkit';
 import { Provider } from 'react-redux';
 import { rootReducer } from './redux';
+
+import { BrowserRouter } from 'react-router-dom';
 /* font Rubik */
-import './components/AddChildForm/AddChildFont.module.css'
+import './components/AddChildForm/AddChildFont.module.css';
 
 /* Style normalize */
 import './assets/normalize.css';
-
 
 const store = configureStore({
   reducer: rootReducer,
 });
 
-ReactDOM.render(  
+ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>,
   document.getElementById('root'),
 );
-
-
-
-
