a39c468799b91810f84e40ecf147a2917acb726d
littwa <littwa777@gmail.com>
2020-11-21 11:44:07 +0200

    process verst

--- src/components/InformationByHabbit/InformationByHabbit.js
+++ src/components/InformationByHabbit/InformationByHabbit.js
@@ -2,12 +2,10 @@ import React from 'react';
 import style from './InformationByHabit.module.css';
 import image_boy from './img/image_boy.svg';
 import image_girl from './img/image_girl.svg';
-
+import iconconfirm from './img/Vector_103_check_mark.svg';
+import iconcross from './img/Vector_104_cross.svg';
 import { connect } from 'react-redux';
-
 // import ChangeOrDelHabbit from '../ChangeOrDelHabbit/ChangeOrDelHabbit';
-
-// const cross = require('./img/Vector_104_cross.svg');
 
 class InformationByHabbit extends React.Component {
   state = { showModalChangeOrDelHabbit: false };
@@ -27,17 +25,41 @@ class InformationByHabbit extends React.Component {
           ...
         </button>
         {/* {this.state.showModalChangeOrDelHabbit && <ChangeOrDelHabbit />} */}
-        <div className={style.avatar_boy}>
-          <img className={style.imgavatar} src={image_boy} alt={'pic'} />
+        <div
+          className={
+            this.props.arrHabbitsByUserTest[0].genderChild === 'male'
+              ? style.avatar_boy
+              : style.avatar_girl
+          }
+        >
+          <img
+            className={style.imgavatar}
+            src={
+              this.props.arrHabbitsByUserTest[0].genderChild === 'male'
+                ? image_boy
+                : image_girl
+            }
+            alt={'pic'}
+          />
         </div>
         <div>
-          <h3 className={style.title}>Title</h3>
+          <h3 className={style.title}>
+            {this.props.arrHabbitsByUserTest[0].nameHabbit}
+          </h3>
           <ul className={style.conteiner}>
             {this.props.arrHabbitsByUserTest[0].sprintHabbit
               .split('')
               .map((el, idx) => {
+                console.log(el);
                 return (
-                  <li key={idx}>
+                  <li
+                    className={
+                      el === '+'
+                        ? style.containerlicheck
+                        : style.containerlinocheck
+                    }
+                    key={idx}
+                  >
                     {this.props.arrHabbitsByUserTest[0].priceHabbit}
                   </li>
                 );
@@ -45,11 +67,15 @@ class InformationByHabbit extends React.Component {
           </ul>
           <p className={style.bonusx}>x1.5</p>
         </div>
-        <div>
+        <div className={style.btn_wrap}>
           <h4 className={style.titleconfirm}>Підтвердження</h4>
-          <div>
-            <button className={style.btnconfirm}></button>
-            <button className={style.btnfailure}></button>
+          <div className={style.conf_cont}>
+            <button className={style.btnconfirm}>
+              <img src={iconconfirm} alt={'pic'}></img>
+            </button>
+            <button className={style.btnfailure}>
+              <img src={iconcross} alt={'pic'}></img>
+            </button>
           </div>
         </div>
       </div>
@@ -60,12 +86,12 @@ class InformationByHabbit extends React.Component {
 const mapStateToProps = (state) => ({
   arrHabbitsByUserTest: [
     {
-      sprintHabbit: '1111111111',
+      sprintHabbit: '++-+111111',
       idChild: '5fb4f73805dba90ca4fbf464',
       nameHabbit: 'football4',
       priceHabbit: 2,
       ownerHabbits: '5fb4f73805dba90ca4fbf464',
-      genderChild: 'female',
+      genderChild: 'male',
     },
     {
       sprintHabbit: '++11111111',
