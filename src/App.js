ebdeb98d842e73358f385d05101efa3226160fce
littwa <littwa777@gmail.com>
2020-11-21 13:42:04 +0200

    kl-51 prepared+

--- src/App.js
+++ src/App.js
@@ -4,24 +4,16 @@ import AddChildForm from './components/AddChildForm/Form';
 import Sidebar from './components/IformationOnChild_(sidebar)/InformationList';
 
 /* AuthPage*/
-import AuthPageNavigation from './components/AuthPageNavigation/AuthPageNavigation'
+import AuthPageNavigation from './components/AuthPageNavigation/AuthPageNavigation';
 import ChangeHabbitForm from './components/ChangeHabbitForm';
 
 import './assets/fonts.css';
 import './assets/basic.css';
 
-import InformationByHabbit from './components/InformationByHabbit/InformationByHabbit';
-
-// function App() {
-//   return <div className="App">Hello</div>;
-// }
+// import InformationByHabbit from './components/InformationByHabbit/InformationByHabbit';
 
 function App() {
-  return (
-    <div className="App">
-      <InformationByHabbit />
-    </div>
-  );
+  return <div className="App">Hello</div>;
 }
 
 export default App;
