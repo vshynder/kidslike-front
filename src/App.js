d78978f7777c5a0fbd7b5a7afc9a8e8aa1d30eec
unknown <yaroslavdutka@gmail.com>
2020-11-24 13:06:36 +0200

    in progress

--- src/App.js
+++ src/App.js
@@ -4,13 +4,16 @@ import AddChildForm from './components/AddChildForm/Form';
 import Sidebar from './components/IformationOnChild_(sidebar)/InformationList';
 
 /* AuthPage*/
-import AuthPageNavigation from './components/AuthPageNavigation/AuthPageNavigation'
+import AuthPageNavigation from './components/AuthPageNavigation/AuthPageNavigation';
 import ChangeHabbitForm from './components/ChangeHabbitForm';
+import Main from './components/MainPage/MainPage';
 
 import './assets/fonts.css';
 
 function App() {
-  return <div className="App">Hello</div>;
+  return <Main></Main>;
+
+  // <div className="App">Hello</div>;
 }
 
 export default App;
--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -14,7 +14,10 @@ export default class MainPosition extends Component {
           <div className="familynfo">
             <Slider></Slider>
           </div>
-          <div className="tasksinfo">ofcoues</div>
+          <div className="habitsInfo">ofcoues</div>
+          <div className="extendMain">
+            <div className="tasksinfo">oops</div>
+          </div>
         </div>
       </div>
     );
