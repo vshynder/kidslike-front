71031f3ac4f1e523bede2ffc5843f823e512c57f
kotiakhova <kotiakhovaa@gmail.com>
2020-11-25 15:53:55 +0200

    InformationByTask layout

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import { Switch, Route } from 'react-router-dom';
 
 import Main from './components/MainPage/MainPage';
 import PrivateRouter from './components/MainPage/PrivateRoutes';
+import InformationByTask from './components/InformationByTask';
 
 import './assets/fonts.css';
 import './assets/basic.css';
+import InformationItem from './components/IformationOnChild_(sidebar)/InformationItem';
 
 const App = () => (
-  <Switch>
-    <PrivateRouter path="/main" exact component={Main} />
-  </Switch>
+  // <Switch>
+  //   <PrivateRouter path="/main" exact component={Main} />
+  // </Switch>
+  <InformationByTask />
 );
 
 export default App;
---
+++ src/components/InformationByTask/index.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import profilePhoto from './profile-example.png';
+import styles from './InformationByTask.module.css';
+import stylingStar from './../../assets/images/Star11.svg';
+import iconconfirm from './../../assets/images/confirm.svg';
+import iconcross from './../../assets/images/cross.svg';
+
+function InformationByTask() {
+  return (
+    <>
+      <div className={styles.container}>
+        <span className={styles.firstSmallContainer}>
+          <span className={styles.childPhotoContainer}>
+            <img
+              src={profilePhoto}
+              alt="profile-image"
+              className={styles.userPhoto}
+            />
+          </span>
+          <span className={styles.taskNameAndCostContainer}>
+            <p className={styles.taskName}>Помити посуд</p>
+            <span className={styles.costContainer}>
+              <img
+                src={stylingStar}
+                alt="styling-star"
+                className={styles.taskCostStar}
+              />
+              <p className={styles.taskCostText}>5</p>
+            </span>
+          </span>
+        </span>
+        <span className={styles.secondSmallContainer}>
+          <span className={styles.timeContainer}>
+            <p className={styles.greyTitle}>Час на виконання:</p>
+            <p className={styles.taskDayText}>1 день</p>
+          </span>
+          <span>
+            <p className={styles.greyTitle}>Підтвердження</p>
+            <span className={styles.confirmationContainer}>
+              <button className={styles.confirmationButton}>
+                <img src={iconconfirm} alt={'pic'}></img>
+              </button>
+              <button className={styles.confirmationButton}>
+                <img src={iconcross} alt={'pic'}></img>
+              </button>
+            </span>
+          </span>
+        </span>
+        <button className={styles.additionButton}>...</button>
+      </div>
+    </>
+  );
+}
+
+export default InformationByTask;
