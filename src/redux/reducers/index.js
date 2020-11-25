b1805d857aa05ecc4c3f02e4ceb7ef8582c7fc89
littwa <littwa777@gmail.com>
2020-11-25 18:19:41 +0200

    test1

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,10 +1,13 @@
 import { combineReducers } from 'redux';
 import { loaderReducer } from './laoderReducer';
 import { dummyReducerAllHabbits } from './allHabbitsReducer';
+import addChildReducer from './addChildReducer';
 
 const rootReducer = combineReducers({
   loader: loaderReducer,
-  dummyReducerAllHabbits,
+  childrens: addChildReducer.childrens,
+  auth: addChildReducer.token, // Заглушка
+  dummyReducerAllHabbits, // Для тестирования логику нужно переиспользовать
 });
 
 export default rootReducer;
