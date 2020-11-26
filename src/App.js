7e42ca728ffc338883eacd165d322d3710736d20
unknown <yaroslavdutka@gmail.com>
2020-11-26 17:12:58 +0200

    presents page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router-dom';
 
 import Main from './components/MainPage/MainPage';
 import PrivateRouter from './components/MainPage/PrivateRoutes';
+import PresentPage from './components/PresentsPage/PresentPage';
 
 import './assets/fonts.css';
 import './assets/basic.css';
@@ -10,6 +11,7 @@ import './assets/basic.css';
 const App = () => (
   <Switch>
     <PrivateRouter path="/main" exact component={Main} />
+    <PrivateRouter path="/presents" exact component={PresentPage} />
   </Switch>
 );
 
