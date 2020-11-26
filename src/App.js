8c2aab5099b9dfcb388ba565b6260d154cab2abb
unknown <yaroslavdutka@gmail.com>
2020-11-26 16:28:15 +0200

    maximum done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import Main from './components/MainPage/MainPage';
+import ChildTaskPage from './components/ChildTaskPage/ChildTaskPage';
 import PrivateRouter from './components/MainPage/PrivateRoutes';
 
 import './assets/fonts.css';
@@ -10,6 +11,7 @@ import './assets/basic.css';
 const App = () => (
   <Switch>
     <PrivateRouter path="/main" exact component={Main} />
+    <PrivateRouter path="/tasks" exact component={ChildTaskPage} />
   </Switch>
 );
 
--- src/components/ChildTaskPage/ChildTaskPage.js
+++ src/components/ChildTaskPage/ChildTaskPage.js
@@ -1,1 +1,49 @@
-import 'ChildTaskPage.css';
+import React, { Component, Fragment } from 'react';
+import Media from 'react-media';
+
+import Slider from '../IformationOnChild_(sidebar)/InformationList';
+
+import './ChildTaskPage.css';
+
+export default class MainPosition extends Component {
+  state = {};
+
+  render() {
+    return (
+      <div className="container">
+        <header className="header"></header>
+
+        <Media
+          queries={{
+            small: '(max-width: 767px)',
+            medium: '(min-width: 768px) and (max-width: 1199px)',
+            large: '(min-width: 1200px)',
+          }}
+        >
+          {(matches) => (
+            <Fragment>
+              {matches.small && (
+                <div className="familynfo">
+                  <div className="tasksinfo"></div>
+                </div>
+              )}
+              {matches.medium && (
+                <div className="familynfo">
+                  <div className="tasksinfo"></div>
+                </div>
+              )}
+              {matches.large && (
+                <div className="main">
+                  <div className="familynfo">
+                    <Slider></Slider>
+                  </div>
+                  <div className="tasksinfo"></div>
+                </div>
+              )}
+            </Fragment>
+          )}
+        </Media>
+      </div>
+    );
+  }
+}
