127b5b71b95268fd96a23a1dd5089111c202fa6f
unknown <yaroslavdutka@gmail.com>
2020-11-27 17:52:29 +0200

    Family Modal Added

--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -5,15 +5,29 @@ import Slider from '../IformationOnChild_(sidebar)/InformationList';
 import InformationByHabbit from '../InformationByHabbit/InformationByHabbit';
 import InformationByTask from '../InformationByTask';
 import './MainPage.css';
+import Modal from '../Modal/Modal';
+import Form from '../AddChildForm/Form';
 
 export default class MainPosition extends Component {
-  state = {};
+  state = { modal: false };
+
+  toggleModal = () => {
+    this.setState((state) => ({ modal: !state.modal }));
+  };
 
   render() {
+    const { modal } = this.state;
+
     return (
       <div className="container">
         <header className="header"></header>
+        {modal && (
+          <Modal onClose={this.toggleModal}>
+            <Form onClick={Form}></Form>
+          </Modal>
+        )}
 
+        <button onClick={this.toggleModal}>push me</button>
         <Media
           queries={{
             small: '(max-width: 767px)',
