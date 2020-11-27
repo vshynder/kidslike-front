7f68aeb84e6f5efaee32c66bbdc1481528b0b2ab
OleynikDima <57680550+OleynikDima@users.noreply.github.com>
2020-11-27 11:07:27 +0200

    add present operation

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -2,12 +2,14 @@ import { combineReducers } from 'redux';
 import { loaderReducer } from './laoderReducer';
 import { dummyReducerAllHabbits } from './allHabbitsReducer';
 import addChildReducer from './addChildReducer';
+import presents from './addPresentReducer'
 
 const rootReducer = combineReducers({
   loader: loaderReducer,
   childrens: addChildReducer.childrens,
   auth: addChildReducer.token, // Заглушка
   dummyReducerAllHabbits, // Для тестирования логику нужно переиспользовать
+  presents:presents,
 });
 
 export default rootReducer;
