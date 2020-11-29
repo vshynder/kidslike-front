714b3c37ff9b039c7b2adde4e2a8b4f03f7e5f8e
Vlad Shynder <shynderv@gmail.com>
2020-11-29 15:14:33 +0200

    redirect to main done

--- src/components/Header/Logo.js
+++ src/components/Header/Logo.js
@@ -1,17 +1,23 @@
 import React from 'react';
-import { useLocation } from 'react-router-dom';
+import { useHistory, useLocation } from 'react-router-dom';
 
 import logoOrange from '../../assets/images/logo-orange.png';
 import logoWhite from '../../assets/images/logo-white.png';
 
 export default function Logo() {
+  const history = useHistory();
   const location = useLocation();
   console.log(location);
   // location.pathname - path v Route
   const whiteLogoLocations = ['/', '/login', '/register'];
 
+  const handleRedirect = () => {
+    history.push('/main');
+  };
+
   return (
     <img
+      onClick={handleRedirect}
       src={
         whiteLogoLocations.includes(location.pathname) ? logoWhite : logoOrange
       }
