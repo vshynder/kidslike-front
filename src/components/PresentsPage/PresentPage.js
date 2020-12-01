4bd5498ef13eaf1f351ec3f6cefae4cf8ad4d96c
unknown <yaroslavdutka@gmail.com>
2020-12-01 17:21:13 +0200

    present page in progress

--- src/components/PresentsPage/PresentPage.js
+++ src/components/PresentsPage/PresentPage.js
@@ -3,36 +3,84 @@ import Media from 'react-media';
 import Header from '../Header';
 
 import Slider from '../IformationOnChild_(sidebar)/InformationList';
+import PresentsIcon from '../../assets/images/presentsIcon.png';
 
 import './PresentPage.css';
 
 export default class MainPosition extends Component {
-  state = {};
+  state = { modal: false, addFormPresent: false };
 
   render() {
     return (
-      <div className="container">
-        <header className="header">
+      <div className="presentcontainer">
+        <header className="presentheader">
           <Header />
         </header>
 
         <Media
           queries={{
             small: '(max-width: 767px)',
-            medium: '(min-width: 768px) and (max-width: 1199px)',
-            large: '(min-width: 1200px)',
+            medium: '(min-width: 768px) and (max-width: 1249px)',
+            large: '(min-width: 1250px)',
           }}
         >
           {(matches) => (
             <Fragment>
-              {matches.small && <div className="presents"></div>}
-              {matches.medium && <div className="presents"></div>}
+              {matches.small && (
+                <div className="presents">
+                  <div className="presents">
+                    <div className="presents_header">
+                      <img className="presents_header-img" src={PresentsIcon} />
+                      <h2 className="presents_header-title">Подарунки</h2>
+                    </div>
+                    <div className="presents_list">
+                      {/* <InformationByHabbit></InformationByHabbit> */}
+                    </div>
+                    <div className="presents_button">
+                      <button className="presents_button-button">
+                        Додати подарунок +
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              )}
+              {matches.medium && (
+                <div className="presents">
+                  <div className="presents">
+                    <div className="presents_header">
+                      <img className="presents_header-img" src={PresentsIcon} />
+                      <h2 className="presents_header-title">Подарунки</h2>
+                    </div>
+                    <div className="presents_list">
+                      {/* <InformationByHabbit></InformationByHabbit> */}
+                    </div>
+                    <div className="presents_button">
+                      <button className="presents_button-button">
+                        Додати подарунок +
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              )}
               {matches.large && (
-                <div className="main">
-                  <div className="familynfo">
+                <div className="presentmain">
+                  <div className="presentfamilynfo">
                     <Slider></Slider>
                   </div>
-                  <div className="presents"></div>
+                  <div className="presents">
+                    <div className="presents_header">
+                      <img className="presents_header-img" src={PresentsIcon} />
+                      <h2 className="presents_header-title">Подарунки</h2>
+                    </div>
+                    <div className="presents_list">
+                      {/* <InformationByHabbit></InformationByHabbit> */}
+                    </div>
+                    <div className="presents_button">
+                      <button className="presents_button-button">
+                        Додати подарунок +
+                      </button>
+                    </div>
+                  </div>
                 </div>
               )}
             </Fragment>
