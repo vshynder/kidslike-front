74a3cb3d64d517b7a33d4824bbfb41dbcfdfcfce
poskachei <vposkachey@gmail.com>
2020-12-03 12:33:55 +0200

    add kl-37

--- src/components/SideBarLeft/index.js
+++ src/components/SideBarLeft/index.js
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styles from './SideBarLeft.module.css';
 
-function SideBarLeft() {
-  return (
+import Modal from '../Modal/Modal';
+import AddChildForm from '../AddChildForm/Form';
+
+export default class  SideBarLeft extends Component {
+  state = {isRenderModal: false };
+
+  handleOpenModal = () => {
+    this.setState({ isRenderModal: true });
+  };
+  handleCloseModal = () => {
+    this.setState({ isRenderModal: false });
+  };
+
+  render() {
+    const { isRenderModal } = this.state;
+    return (
     <>
       <div className={styles.container}>
         <div className={styles.heading}>
@@ -17,10 +31,19 @@ function SideBarLeft() {
           <div className={styles.childForm}></div>
         </div>    
         
-          <button className={styles.btnAddBaby}>Додати дитину +</button>
-    
+          <button
+            onClick={this.handleOpenModal} 
+            className={styles.btnAddBaby}
+          >
+            Додати дитину +
+          </button>
+            {isRenderModal && (
+              <Modal  onClose={this.handleCloseModal}>
+                <AddChildForm onClose={this.handleCloseModal} />
+              </Modal>
+            )} 
       </div>
     </>
-  );
+    );
+  };
 }
-export default SideBarLeft;
