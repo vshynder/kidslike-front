923bd495772ccd259f2c9ea3f4510c2a0e899ac9
OleynikDima <57680550+OleynikDima@users.noreply.github.com>
2020-12-13 23:02:13 +0200

    get cookie log

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 import { Provider } from 'react-redux';
-import { store ,persistor} from './redux';
-import { PersistGate } from 'redux-persist/integration/react'
-
+import { store, persistor } from './redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { CookiesProvider } from 'react-cookie';
 
 import { BrowserRouter } from 'react-router-dom';
 /* font Rubik */
@@ -14,14 +14,15 @@ import './components/AddChildForm/AddChildFont.module.css';
 /* Style normalize */
 import './assets/normalize.css';
 
-
 ReactDOM.render(
   <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
+    <CookiesProvider>
+      <PersistGate loading={null} persistor={persistor}>
         <BrowserRouter>
-              <App />
+          <App />
         </BrowserRouter>
-    </PersistGate>
+      </PersistGate>
+    </CookiesProvider>
   </Provider>,
   document.getElementById('root'),
 );
